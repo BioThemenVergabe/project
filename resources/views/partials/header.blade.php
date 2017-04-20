bf14c5b11ad44dd26b193899118193aa1890a71b
Patrick-Laptop\Patrick <patrick.moeser@gmx.net>
2017-04-20 18:13:57 +0200

    see #23

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -8,7 +8,7 @@
                     <a href="/logout" class="btn btn-danger btn-sm icon icon-log-out"></a>
                 </div>
             </nav>
-            <label id="logo"><span class="hidden-xs hidden-sm">{{ config('app.name') }}</span>&nbsp;</label>
+            <a href="{{ url('/') }}"><label id="logo"><span class="hidden-xs hidden-sm">{{ config('app.name') }}</span>&nbsp;</label></a>
         </div>
     </div>
 </header>

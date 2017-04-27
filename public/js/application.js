02afb9177010bd111abd27bbe3ac7f92ddd4ba26
Patrick-Laptop\Patrick <patrick.moeser@gmx.net>
2017-04-27 16:15:05 +0200

    closes #27

--- public/js/application.js
+++ public/js/application.js
@@ -18,25 +18,8 @@ $(function() {
         $('#'+$(this).data('action')).modal();
     });
 
-/*
-    $('[type=password]').keyup(function(e) {
-        var parent = $(this).parents('.form-group');
-        var sbmt = $(this).parents('form').find('[type=submit]');
-        if($(this).val().length < 8) {
-            if(!parent.hasClass('has-error'));
-            parent.addClass('has-error');
-            if(!sbmt.hasClass('disabled'))
-                sbmt.addClass('disabled');
-        } else {
-            if(parent.hasClass('has-error'))
-                parent.removeClass('has-error');
-            if(sbmt.hasClass('disabled'))
-                sbmt.removeClass('disabled');
-        }
-    });
-*/
-
-    $('[type=reset]').click(function() {
+    $('[type=reset]').click(function(e) {
+        $(this).closest('form').get(0).reset();
         var sum = 0;
         $('[data-target=range]').each(function() {
            $(this).html(function() {

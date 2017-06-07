893758f439af369d9a7cbe5d78049719876c640e
Matthias\DESKTOP-SC296V2 <mat.leo@web.de>
2017-06-07 17:05:12 +0200

    bugfix

--- resources/views/ajax/admin_statusfield.blade.php
+++ resources/views/ajax/admin_statusfield.blade.php
@@ -1,7 +1,9 @@
-<div class="col-sm-4 col-md-4 pull-right">
-    @if($status=="open")
-        <span class="icon icon-check"></span> @lang('content.admin_dash5')
-            @else
-                <span class="icon icon-cross"></span> @lang('content.admin_dash6')
-                    @endif
+<div class="col-sm-3 col-md-2 col-xs-4">
+        <div class="pull-right">
+                @if($status=="open")
+                        <span class="icon icon-check"></span> @lang('content.admin_dash5')
+                @else
+                        <span class="icon icon-cross"></span> @lang('content.admin_dash6')
+                @endif
+        </div>
 </div>

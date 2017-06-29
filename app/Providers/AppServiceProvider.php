c8cf1bcee2fd2ddb7879c6e4e703b9952aa10cd7
Patrick-Laptop\Patrick <patrick.moeser@gmx.net>
2017-06-29 13:38:26 +0200

    .

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,28 +21,27 @@ class AppServiceProvider extends ServiceProvider
             $curUser = Auth::user();
             $user = NULL;
             if ($attributes == "matrnr") {
-                if($curUser->matrnr == $value)
+                if ($curUser->matrnr == $value)
                     return true;
-                $user = User::where('matrnr','=',$value)->get();
+                $user = User::where('matrnr', '=', $value)->get();
             } elseif ($attributes == "email") {
-                if($curUser->email == $value)
+                if ($curUser->email == $value)
                     return true;
-                $user = User::where('email','=',$value)->get();
+                $user = User::where('email', '=', $value)->get();
             }
-            if(is_null($user))
+            if (is_null($user) || count($user) == 0)
                 return true;
             return false;
         });
     }
 
-/**
- * Register any application services.
- *
- * @return void
- */
-public
-function register()
-{
-    //
-}
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
 }
